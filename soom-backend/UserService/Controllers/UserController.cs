bf0d0ffde97e94398605d56aa4b7301757b6b022
Mohammed Alsaeed <saeedalsaeedbusiness@gmail.com>
2025-08-24 15:16:07 +0300

    Initial commit: add user controller and User microservice

---
+++ soom-backend/UserService/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Data;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+        
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost("subscribe")]
+        public async Task<IActionResult> Subscribe([FromBody] string email)
+        {
+            var user = new User { Email = email };  
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Email subscribed successfully", email });
+        }
+    }
+}
---
+++ soom-backend/UserService/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql("Host=localhost;Database=soom-db;Username=postgres;Password=postgres"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
